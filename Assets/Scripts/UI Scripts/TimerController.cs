ba82019a5124df46d998cd0987e0e64fb3f44982
Silver Burnie <AuAgments@gmail.com>
2024-10-17 12:04:51 -0300

    Win/Lose, Numbers UI, new Character and Boss assests added
    Time UI numbers are functional, counting down from 100 seconds by replacing placeholder images with the corresponding time signature.
    Imported Win/Lose UI assets - not yet implemented.
    Imported new Character and Boss - not yet implemented

--- Assets/Scripts/UI Scripts/TimerController.cs
+++ Assets/Scripts/UI Scripts/TimerController.cs
@@ -1,19 +1,28 @@
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class TimerController : MonoBehaviour
 {
+    public Image[] NumbersImages;
+
+    public Image Ones;
+    public Image Tens;
+    public Image Hundreds;
     public Image fillImage;
 
     public float duration = 10f;
-
     public float elapsedTime = 0f;
+
+    private int lastDisplayedTime = -1;
+    int remainingTime;
 
     public static bool TimerOver = false;
 
     void Start()
     {
         fillImage.fillAmount = 1.0f;
+        SetTimerUINumbers(Mathf.CeilToInt(duration - elapsedTime));
     }
 
     void Update()
@@ -21,13 +30,33 @@ public class TimerController : MonoBehaviour
         elapsedTime += Time.deltaTime;
 
         float fillValue = Mathf.Lerp(1.0f, 0.0f, elapsedTime / duration);
-
         UpdateFillAmount(fillValue);
+
+        remainingTime = Mathf.CeilToInt(duration - elapsedTime);
+        Debug.Log(remainingTime.ToString());
+
+        if (remainingTime != lastDisplayedTime && remainingTime >= 0)
+        {
+            SetTimerUINumbers(remainingTime);
+            lastDisplayedTime = remainingTime;
+        }
+
 
         if (elapsedTime > duration)
         { 
-        TimerOver = true;
+            TimerOver = true;
         }
+    }
+
+    private void SetTimerUINumbers(int time)
+    {
+        int ones = time % 10;
+        int tens = (time / 10) % 10;
+        int hundreds = (time / 100) % 10;
+
+        Ones.sprite = NumbersImages[ones].sprite; // Access the sprite property to set the image
+        Tens.sprite = NumbersImages[tens].sprite;
+        Hundreds.sprite = NumbersImages[hundreds].sprite;
     }
 
     public void UpdateFillAmount(float value)
