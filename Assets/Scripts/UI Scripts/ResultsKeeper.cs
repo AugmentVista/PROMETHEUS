12b6fc1939aa439b384ebbf54d6c42e67d762ae0
Silver Burnie <AuAgments@gmail.com>
2024-12-05 14:39:51 -0400

    Victory screen update
    Victory screen and Results Keeper script no longer supports points.
    Added Thousandths place for earnings.
    Victory screen wave number set to 20 as it should never not be 20 when you reach win screen

--- Assets/Scripts/UI Scripts/ResultsKeeper.cs
+++ Assets/Scripts/UI Scripts/ResultsKeeper.cs
@@ -5,25 +5,17 @@ public class ResultsKeeper : MonoBehaviour
 {
     [SerializeField] private ScoreKeeper scoreKeeper;
 
-    public Image[] PointsNumberImages;
-
-    public Image pointsOnes;
-    public Image pointsTens;
-    public Image pointsHundreds;
-
     public Image[] EarningsNumberImages;
 
     public Image earningsOnes;
     public Image earningsTens;
-    public Image earningsHundreds;  
+    public Image earningsHundreds;
+    public Image earningsThousands;
 
     public Image[] WaveNumberImages;
 
     public Image waveOnes;
     public Image waveTens;
-
-
-    private int score => scoreKeeper != null ? scoreKeeper.score : 0;
     private int earnings => scoreKeeper != null ? scoreKeeper.drachma : 0;
 
     public int GetWaveInfo()
@@ -33,32 +25,24 @@ public class ResultsKeeper : MonoBehaviour
 
     public void ShowResults() 
     {
-        Debug.Log("Points is " + score);
         Debug.Log("money is " + earnings);
         Debug.LogError($"wave is: {GetWaveInfo().ToString()}");
-        DetermineResults(score, earnings, GetWaveInfo());
     }
 
     private void DetermineResults(int points, int money, int waveCount)
     {
-        int pOnes = points % 10;
-        int pTens = (points / 10) % 10;
-        int pHundreds = (points /  100) % 10;
-
         int eOnes = money % 10;
         int eTens = (money / 10) % 10;
         int eHundreds = (money / 100) % 10;
+        int eThousands = (money / 1000) % 10;
 
         int wOnes = waveCount % 10;
         int wTens = (waveCount / 10) % 10;
 
-        pointsOnes.sprite = PointsNumberImages[pOnes].sprite;
-        pointsTens.sprite = PointsNumberImages[pTens].sprite;
-        pointsHundreds.sprite = PointsNumberImages[pHundreds].sprite;
-
         earningsOnes.sprite = EarningsNumberImages[eOnes].sprite;
         earningsTens.sprite = EarningsNumberImages[eTens].sprite;
-        earningsHundreds.sprite = EarningsNumberImages[pHundreds].sprite;
+        earningsHundreds.sprite = EarningsNumberImages[eHundreds].sprite;
+        earningsThousands.sprite = EarningsNumberImages[eThousands].sprite;
 
         waveOnes.sprite = WaveNumberImages[wOnes].sprite;
         waveTens.sprite = WaveNumberImages[wTens].sprite;
